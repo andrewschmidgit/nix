b522fe87007b94bbe004306875e17cf6b8579d6b
Andrew Schmid <andrewschmid-dev@proton.me>
2025-05-24 19:42:10 -0500

    update tmux settings

--- modules/home-manager/programs/tmux/default.nix
+++ modules/home-manager/programs/tmux/default.nix
@@ -1,10 +1,12 @@
 { ... }: {
   programs.tmux = {
     enable = true;
-    shortcut = "a";
-    mouse = true;
     baseIndex = 1;
     escapeTime = 10;
+    historyLimit = 10000;
     keyMode = "vi";
+    shortcut = "a";
+    mouse = true;
+    terminal = "tmux-256color";
   };
 }
