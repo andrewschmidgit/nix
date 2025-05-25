eb55be4e74baf464efbd9393470ad7689b39c8a0
Andrew Schmid <andrewschmid-dev@proton.me>
2025-05-24 22:27:55 -0500

    add sway screenshots

--- modules/home-manager/programs/sway/default.nix
+++ modules/home-manager/programs/sway/default.nix
@@ -1,19 +1,31 @@
 {
+  config,
   pkgs,
+  lib,
   ...
 }:
 
 {
-  programs.kitty.enable = true;
+  programs.alacritty.enable = true;
 
   home.packages = with pkgs; [
     grim
+    slurp
+    wl-clipboard
+    mako
+    shotman
   ];
 
   wayland.windowManager.sway = {
     enable = true;
-    config = {
-      terminal = "kitty";
+    config = rec {
+      terminal = "alacritty";
+      keybindings = let
+      	mod = config.wayland.windowManager.sway.config.modifier;
+      in lib.mkOptionDefault {
+      	"${mod}+Backspace" = "exec shotman -c output";
+      	"${mod}+Shift+Backspace" = "exec shotman -c region";
+      };
     };
   };
 }
