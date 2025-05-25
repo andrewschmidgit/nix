c9b5d163f1c3a55532aa322177f583820c97d485
Andrew Schmid <andrewschmid-dev@proton.me>
2025-05-24 22:25:45 -0500

    force sway in vm to be half-screen

--- home/andrew/albatross/default.nix
+++ home/andrew/albatross/default.nix
@@ -7,4 +7,10 @@
   programs.home-manager.enable = true;
 
   home.stateVersion = "24.11";
+
+  wayland.windowManager.sway.config.output = {
+    "Virtual-1" = {
+      mode = "1920x1440@60Hz";
+    };
+  };
 }
