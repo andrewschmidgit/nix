42416f09952838c5ca11ffde5c60e9a43bbf5c20
Andrew Schmid <andrewschmid-dev@proton.me>
2025-05-25 20:30:33 -0500

    add agenix

--- flake.nix
+++ flake.nix
@@ -7,10 +7,15 @@
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    agenix.url = "github:ryantm/agenix";
+    agenix.inputs.nixpkgs.follows = "nixpkgs";
+    agenix.inputs.darwin.follows = ""; # don't download darwin deps
   };
 
   outputs = {
     self,
+    agenix,
     nixpkgs,
     home-manager,
     ... }@inputs:
@@ -18,7 +23,10 @@
     mkNixosConfiguration = {
       hostname, # required string
       username, # required string
+      system ? "x86_64-linux"
     }: nixpkgs.lib.nixosSystem {
+        inherit system;
+
         specialArgs = {
           inherit inputs hostname username;
           nixosModules = "${self}/modules/nixos";
@@ -26,7 +34,12 @@
 
         modules = [
           ./hosts/${hostname}
-	  ./users/${username}/nixos
+          ./users/${username}/nixos.nix
+
+          agenix.nixosModules.default
+          { environment.systemPackages = [ agenix.packages.${system}.default ]; }
+
+
           home-manager.nixosModules.home-manager {
             home-manager.users.${username} = import ./users/${username};
 
